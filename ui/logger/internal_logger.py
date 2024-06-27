34e40c95b470d2abb6120ab1c9aa96058856442d
ViNN280801 <vladislavsemykin@loveit.loveit>
2024-06-27 09:40:11 +0300

    Improved logger -> added traceback mechanism to trace from what moment exception case raised

--- ui/logger/internal_logger.py
+++ ui/logger/internal_logger.py
@@ -3,18 +3,27 @@ class InternalLogger:
     @staticmethod
     def pretty_function_details() -> str:
         """
-        Prints the details of the calling function in the format:
+        Prints the details of the calling function stack in the format:
         <file name>: line[<line number>]: <func name>(<args>)
         """
         from inspect import currentframe, getargvalues
-
+        
+        stack_details = []
         current_frame = currentframe()
-        caller_frame = current_frame.f_back.f_back
-        function_name = caller_frame.f_code.co_name
-        args, _, _, values = getargvalues(caller_frame)
-        function_file = caller_frame.f_code.co_filename
-        formatted_args = ', '.join([f"{arg}={values[arg]}" for arg in args])
-        return f"{function_file}: {function_name}({formatted_args})"
+        caller_frame = current_frame.f_back
+
+        idx = 0
+        while caller_frame is not None:
+            function_name = caller_frame.f_code.co_name
+            args, _, _, values = getargvalues(caller_frame)
+            function_file = caller_frame.f_code.co_filename
+            function_line_no = caller_frame.f_lineno
+            formatted_args = ', '.join([f"{arg}={values[arg]}" for arg in args])
+            stack_details.append(f"Call {idx}: {function_file}: line[{function_line_no}]: {function_name}({formatted_args})")
+            caller_frame = caller_frame.f_back
+            idx += 1
+
+        return "\n".join(stack_details)
 
     @staticmethod
     def get_warning_none_result():
