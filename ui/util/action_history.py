ec0f3238acf06697d25eea1af5157a46b6542ea1
ViNN280801 <vladislavsemykin@loveit.loveit>
2024-07-04 15:58:42 +0300

    Corrected action history according to the tests

--- ui/util/action_history.py
+++ ui/util/action_history.py
@@ -1,6 +1,6 @@
 class ActionHistory:
     def __init__(self):
-        self.id = 0           # Counter for the current ID of objects
+        self._id = 0          # Counter for the current ID of objects
         self.undo_stack = {}  # Stack to keep track of undo actions
         self.redo_stack = {}  # Stack to keep track of redo actions
 
@@ -8,31 +8,31 @@ class ActionHistory:
         """
         Add a new action to the history. This clears the redo stack.
         """
-        self.undo_stack[self.id] = object_on_stack
+        self.undo_stack[self._id] = object_on_stack
 
     def undo(self):
         """
         Undo the last action.
         Returns the row_id and actors for the undone action.
         """
-        if not self.undo_stack:
-            return None
-        last_id = max(self.undo_stack.keys())
-        object_on_stack = self.undo_stack.pop(last_id)
-        self.redo_stack[last_id] = object_on_stack
-        return object_on_stack
+        if self._id - 1 in self.undo_stack:
+            self._id -= 1
+            action = self.undo_stack.pop(self._id)
+            self.redo_stack[self._id] = action
+            return action
+        return None
 
     def redo(self):
         """
         Redo the last undone action.
         Returns the row_id and actors for the redone action.
         """
-        if not self.redo_stack:
-            return None
-        last_id = max(self.redo_stack.keys())
-        object_on_stack = self.redo_stack.pop(last_id)
-        self.undo_stack[last_id] = object_on_stack
-        return object_on_stack
+        if self._id in self.redo_stack:
+            action = self.redo_stack.pop(self._id)
+            self.undo_stack[self._id] = action
+            self._id += 1
+            return action
+        return None
 
     def remove_by_id(self, id: int):
         """
@@ -44,13 +44,14 @@ class ActionHistory:
             del self.redo_stack[id]
 
     def get_id(self):
-        return self.id
+        return self._id
 
     def decrementIndex(self):
-        self.id -= 1
+        if self._id > 0:
+            self._id -= 1
 
     def incrementIndex(self):
-        self.id += 1
+        self._id += 1
 
     def clearIndex(self):
-        self.id = 0
+        self._id = 0
