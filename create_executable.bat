995c94c9ce2b477393dc0d570ce6284f4288511d
ViNN280801 <vladislavsemykin@loveit.loveit>
2024-07-16 10:36:24 +0300

    More descriptive name of the output file

--- create_executable.bat
+++ create_executable.bat
@@ -5,7 +5,7 @@ set current_date=%datetime:~6,2%.%datetime:~4,2%.%datetime:~0,4%
 set output_name=nia_start_%current_date%
 
 pyinstaller --onefile --add-binary ".\Release\nia_start.exe;." --add-binary ".\Release\gmsh-4.12.dll;." --add-binary ".\Release\libgmp-10.dll;." --add-data ".\icons;icons" --paths ".\ui;.\include" --name %output_name% ui\main.py
-pyinstaller nia_start.spec
+pyinstaller %output_name%.spec
 
 xcopy /E /I .\icons .\dist\icons
 copy .\Release\nia_start.exe .\dist\nia_start.exe
--- create_executable.sh
+++ create_executable.sh
@@ -5,7 +5,7 @@ output_name="nia_start_$current_date"
 
 ./compile.sh -j 4
 pyinstaller --onefile --add-binary='./nia_start:.' --add-data='./icons:icons' --paths=./ui --name "$output_name" ui/main.py
-pyinstaller nia_start.spec
+pyinstaller $output_name.spec
 
 cp -rv icons/ dist/icons
 cp -v nia_start dist/nia_start
