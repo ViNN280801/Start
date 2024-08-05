3911df5c21057c694d6772ecf28a61b3450205e6
ViNN280801 <vladislavsemykin@loveit.loveit>
2024-08-05 19:54:54 +0300

    Fixed script to build Dockerfile to image, then run application. Added some advices

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# Adding docker to the host X-server to turn on the graphical env
 xhost +local:docker
+
+# Build the Docker image
 docker build -t startim .
-docker run -it -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /dev/dri:/dev/dri startim /bin/bash -c "./nia_start"
+
+# Rebuild container from scratch each time. (2>/dev/null || true) avoid failure if in we have error in `stderr`
+docker stop start_container 2>/dev/null || true
+docker rm start_container 2>/dev/null || true
+
+# Run the Docker container with graphical environment support
+docker run -it --name start_container -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /dev/dri:/dev/dri startim
+
+# To remove this image and container execute the following commands
+# docker rm start_container 2>/dev/null || true
+# docker rmi startim 2>/dev/null || true
+
+# To push image to the dockerhub:
+# docker login
+# docker tag startim <name_of_the_image_in_dockerhub>
+# docker push <name_of_the_image_in_dockerhub>
