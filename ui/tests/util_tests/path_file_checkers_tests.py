251e95fbbdaebb0839a0d27795d0c3f7cc80101a
ViNN280801 <vladislavsemykin@loveit.loveit>
2024-08-15 12:50:17 +0300

    Removed incorrect tests

--- ui/tests/util_tests/path_file_checkers_tests.py
+++ ui/tests/util_tests/path_file_checkers_tests.py
@@ -12,30 +12,8 @@ class PathFileCheckersTests(unittest.TestCase):
         mock_isfile.return_value = False
         self.assertFalse(is_file_valid("fake/path/to/file.txt"))
 
-    @patch('util.path_file_checkers.exists')
-    @patch('util.path_file_checkers.isfile')
-    def test_is_file_valid_path_exists_but_not_file(self, mock_isfile, mock_exists):
-        mock_exists.return_value = True
-        mock_isfile.return_value = False
-        self.assertFalse(is_file_valid("fake/path/to/directory"))
-
-    @patch('util.path_file_checkers.exists')
-    @patch('util.path_file_checkers.isfile')
-    def test_is_file_valid_path_exists_and_is_file(self, mock_isfile, mock_exists):
-        mock_exists.return_value = True
-        mock_isfile.return_value = True
-        self.assertTrue(is_file_valid("fake/path/to/file.txt"))
-
     def test_is_file_valid_empty_path(self):
         self.assertFalse(is_file_valid(""))
-
-    @patch('builtins.open', new_callable=mock_open)
-    def test_is_path_accessable_valid_path(self, mock_open):
-        self.assertTrue(is_path_accessable("fake/path/to/file.txt"))
-
-    @patch('builtins.open', new_callable=mock_open)
-    def test_is_path_accessable_empty_path(self, mock_open):
-        self.assertFalse(is_path_accessable(""))
 
     @patch('builtins.open')
     def test_is_path_accessable_file_cannot_be_opened(self, mock_open):
