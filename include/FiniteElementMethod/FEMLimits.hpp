3584ecdf8fcc57b3fa8185785d1c0f7163fdc619
ViNN280801 <vladislavsemykin@loveit.loveit>
2024-09-09 11:42:51 +0300

    Added some usefull constants for restrict polynomial order and desired calculation accuracy

---
+++ include/FiniteElementMethod/FEMLimits.hpp
@@ -0,0 +1,14 @@
+#ifndef FEMLIMITS_HPP
+#define FEMLIMITS_HPP
+
+static constinit const unsigned short FEM_LIMITS_NULL_VALUE{0U};
+
+static constinit const unsigned short FEM_LIMITS_DEFAULT_POLYNOMIAL_ORDER{1U};
+static constinit const unsigned short FEM_LIMITS_MIN_POLYNOMIAL_ORDER{1U};
+static constinit const unsigned short FEM_LIMITS_MAX_POLYNOMIAL_ORDER{20U};
+
+static constinit const unsigned short FEM_LIMITS_DEFAULT_DESIRED_CALCULATION_ACCURACY{3U};
+static constinit const unsigned short FEM_LIMITS_MIN_DESIRED_CALCULATION_ACCURACY{1U};
+static constinit const unsigned short FEM_LIMITS_MAX_DESIRED_CALCULATION_ACCURACY{20U};
+
+#endif // !FEMLIMITS_HPP
