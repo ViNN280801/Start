a0b85e6bec89493b13103905df978b391d879b00
ViNN280801 <vladislavsemykin@loveit.loveit>
2024-09-09 14:06:11 +0300

    Corrected orphographical error

--- src/FiniteElementMethod/FEMCheckers.cpp
+++ src/FiniteElementMethod/FEMCheckers.cpp
@@ -7,7 +7,7 @@ void FEMCheckers::checkDesiredAccuracy(short desired_accuracy)
     if (desired_accuracy < FEM_LIMITS_NULL_VALUE)
         throw std::underflow_error("Desired calculation accuracy can't be negative");
     if (desired_accuracy == FEM_LIMITS_NULL_VALUE)
-        throw std::invalid_argument("Desired calculation accuracy can't be eqaul to 0");
+        throw std::invalid_argument("Desired calculation accuracy can't be equal to 0");
     if (desired_accuracy > FEM_LIMITS_MAX_DESIRED_CALCULATION_ACCURACY)
         throw std::overflow_error(std::format("Desired calculation accuracy can't be greater than {}. Required range: [{}; {}]",
                                               FEM_LIMITS_MAX_DESIRED_CALCULATION_ACCURACY,
@@ -19,14 +19,14 @@ void FEMCheckers::checkPolynomOrder(short polynom_order)
     if (polynom_order < FEM_LIMITS_NULL_VALUE)
         throw std::underflow_error("Desired calculation accuracy can't be negative");
     if (polynom_order == FEM_LIMITS_NULL_VALUE)
-        throw std::invalid_argument("Desired calculation accuracy can't be eqaul to 0");
+        throw std::invalid_argument("Desired calculation accuracy can't be equal to 0");
     if (polynom_order > FEM_LIMITS_MAX_POLYNOMIAL_ORDER)
         throw std::overflow_error(std::format("Desired calculation accuracy can't be greater than {}. Required range: [{}; {}]",
                                               FEM_LIMITS_MAX_POLYNOMIAL_ORDER,
                                               FEM_LIMITS_MIN_POLYNOMIAL_ORDER, FEM_LIMITS_MAX_POLYNOMIAL_ORDER));
 }
 
-void FEMCheckers::checkCell(CellType cellType)
+void FEMCheckers::checkCellType(CellType cellType)
 {
     switch (cellType)
     {
