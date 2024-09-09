f2e52304529c601946600f3f15c57a40ffc87226
ViNN280801 <vladislavsemykin@loveit.loveit>
2024-09-09 15:32:29 +0300

    Added new constants for cell types and default space dimension (3D)

--- include/FiniteElementMethod/FEMLimits.hpp
+++ include/FiniteElementMethod/FEMLimits.hpp
@@ -11,4 +11,19 @@ static constinit const unsigned short FEM_LIMITS_DEFAULT_DESIRED_CALCULATION_ACC
 static constinit const unsigned short FEM_LIMITS_MIN_DESIRED_CALCULATION_ACCURACY{1U};
 static constinit const unsigned short FEM_LIMITS_MAX_DESIRED_CALCULATION_ACCURACY{20U};
 
+// **************************** Count of vertices of 2D cells. *********************************** //
+static constinit const unsigned short FEM_LIMITS_DEFAULT_TRIANGLE_VERTICES_COUNT{3U}; // 2D triangle.
+static constinit const unsigned short FEM_LIMITS_DEFAULT_PENTAGON_VERTICES_COUNT{5U}; // 2D pentagon.
+static constinit const unsigned short FEM_LIMITS_DEFAULT_HEXAGON_VERTICES_COUNT{5U};  // 2D hexagon.
+// *********************************************************************************************** //
+
+// ******************************* Count of vertices of 3D cells. ************************************** //
+static constinit const unsigned short FEM_LIMITS_DEFAULT_TETRAHEDRON_VERTICES_COUNT{4U}; // 3D tetrahedron.
+static constinit const unsigned short FEM_LIMITS_DEFAULT_PYRAMID_VERTICES_COUNT{5U};     // 3D pyramid.
+static constinit const unsigned short FEM_LIMITS_DEFAULT_WEDGE_VERTICES_COUNT{6U};       // 3D wedge.
+static constinit const unsigned short FEM_LIMITS_DEFAULT_HEXAHEDRON_VERTICES_COUNT{4U};  // 3D hexahedron.
+// ***************************************************************************************************** //
+
+static constinit const unsigned short FEM_LIMITS_DEFAULT_SPACE_DIMENSION{3U};
+
 #endif // !FEMLIMITS_HPP
