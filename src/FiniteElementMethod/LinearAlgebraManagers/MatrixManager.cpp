2601ee7aedf307e6a742a43338bfe5e22d93a9c1
ViNN280801 <vladislavsemykin@loveit.loveit>
2024-09-10 17:06:07 +0300

    Corrected initializing of the CRS matrix according to the tests: added additional checking for the input and for indices

--- src/FiniteElementMethod/LinearAlgebraManagers/MatrixManager.cpp
+++ src/FiniteElementMethod/LinearAlgebraManagers/MatrixManager.cpp
@@ -1,24 +1,40 @@
-#include "FiniteElementMethod/LinearAlgebraManagers/MatrixManager.hpp"
+#include <algorithm>
+#include <ranges>
+
 #include "FiniteElementMethod/FEMCheckers.hpp"
+#include "FiniteElementMethod/LinearAlgebraManagers/MatrixManager.hpp"
 
 MatrixManager::MatrixManager(std::vector<MatrixEntry> const &matrix_entries) : m_entries(matrix_entries)
 {
+    if (matrix_entries.empty())
+        throw std::invalid_argument("Matrix entries can't be empty");
+
+    if (std::ranges::any_of(matrix_entries, [](MatrixEntry const &entry)
+                            { return entry.row < 0 || entry.col < 0; }))
+        throw std::out_of_range("Matrix entries cannot have negative row or column indices");
+
     // 1. Getting unique global entries.
     std::map<GlobalOrdinal, std::set<GlobalOrdinal>> graphEntries;
     for (auto const &entry : m_entries)
         graphEntries[entry.row].insert(entry.col);
 
-    // 2. Initializing all necessary variables.
-    short indexBase{};
-    auto countGlobalNodes{graphEntries.size()};
+    // 2. Find the maximum global row index.
+    auto maxGlobalIndex = std::ranges::max_element(matrix_entries, {}, &MatrixEntry::row)->row;
 
     // 3. Initializing tpetra map.
-    m_map = Teuchos::rcp(new MapType(countGlobalNodes, indexBase, Tpetra::getDefaultComm()));
+    short indexBase{};
+    m_map = Teuchos::rcp(new MapType(maxGlobalIndex + 1, indexBase, Tpetra::getDefaultComm()));
 
     // 4. Initializing tpetra graph.
-    std::vector<size_t> numEntriesPerRow(countGlobalNodes);
+    std::vector<size_t> numEntriesPerRow(maxGlobalIndex + 1);
     for (auto const &rowEntry : graphEntries)
-        numEntriesPerRow.at(m_map->getLocalElement(rowEntry.first)) = rowEntry.second.size();
+    {
+        auto localIndex = m_map->getLocalElement(rowEntry.first);
+        if (static_cast<size_t>(localIndex) == Teuchos::OrdinalTraits<size_t>::invalid())
+            throw std::out_of_range("Invalid local index for row entry");
+
+        numEntriesPerRow.at(localIndex) = rowEntry.second.size();
+    }
 
     Teuchos::RCP<Tpetra::CrsGraph<>> graph{Teuchos::rcp(new Tpetra::CrsGraph<>(m_map, Teuchos::ArrayView<size_t const>(numEntriesPerRow.data(), numEntriesPerRow.size())))};
     for (auto const &rowEntries : graphEntries)
@@ -32,7 +48,15 @@ MatrixManager::MatrixManager(std::vector<MatrixEntry> const &matrix_entries) : m
     // 5. Initializing matrix.
     m_matrix = Teuchos::rcp(new TpetraMatrixType(graph));
 
-    // 6. Finalizing work with matrix.
+    // 6. Adding matrix entries.
+    for (auto const &entry : m_entries)
+    {
+        Teuchos::ArrayView<GlobalOrdinal const> colsView(std::addressof(entry.col), 1);
+        Teuchos::ArrayView<Scalar const> valsView(std::addressof(entry.value), 1);
+        m_matrix->sumIntoGlobalValues(entry.row, colsView, valsView);
+    }
+
+    // 7. Finalizing work with matrix.
     m_matrix->fillComplete();
 }
 
