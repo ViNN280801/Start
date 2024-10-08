f86ce849ee6d259ae561f665a042a33a5c98e395
ViNN280801 <vladislav_semykin01@mail.ru>
2024-10-08 22:26:12 +0300

    Removed ctor with bool param from CellCelectorException, removed unnecessory macros for the contacting

--- src/FiniteElementMethod/Cell/CellSelectorException.cpp
+++ src/FiniteElementMethod/Cell/CellSelectorException.cpp
@@ -2,14 +2,4 @@
 
 CellSelectorException::CellSelectorException(std::string const &message) : m_message(message) {}
 
-CellSelectorException::CellSelectorException(bool contact_support, std::string const &message)
-{
-    if (contact_support)
-    {
-        m_message = CONTACT_SUPPORT_MSG(message);
-    }
-    else
-        m_message = message;
-}
-
 const char *CellSelectorException::what() const noexcept { return m_message.c_str(); }
