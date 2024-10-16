69c2e9e9be58d38902609f32fcc61ee72c9ffbec
ViNN280801 <vladislav_semykin01@mail.ru>
2024-10-16 11:31:23 +0300

    Added macro for the 'constexpr' STL funcs which are not constexr in C++17, but constexpr in c++20

--- include/Utilities/PreprocessorUtils.hpp
+++ include/Utilities/PreprocessorUtils.hpp
@@ -8,22 +8,22 @@
 #endif
 
 #if __cplusplus >= 202002L
+    #include <numbers>
+
     #define STARTCONSTINIT constinit
+    #define START_PI_NUMBER std::numbers::pi
+    #define STARTCONSTEXPRFUNC constexpr
 #else
     #define STARTCONSTINIT
+    #define STARTCONSTEXPRFUNC
+
+    constexpr double START_PI_NUMBER = 3.14159265358979323846;
 #endif
 
 #ifdef __linux__
     #define COMMON_PRETTY_FUNC __PRETTY_FUNCTION__
 #elif defined(_WIN32)
     #define COMMON_PRETTY_FUNC __FUNCSIG__
-#endif
-
-#if __cplusplus >= 202002L
-    #include <numbers>
-    #define START_PI_NUMBER std::numbers::pi
-#else
-    constexpr double START_PI_NUMBER = 3.14159265358979323846;
 #endif
 
 #endif // !COMPILERUTILS_HPP
