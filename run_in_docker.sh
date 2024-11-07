73081a06680d061b0047d52608440fabc2d8c5d3
ViNN280801 <vladislav_semykin01@mail.ru>
2024-11-07 15:10:22 +0300

    Using variables to make script more convenient in maintenance
    Signed-off-by: ViNN280801 <vladislav_semykin01@mail.ru>

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
+DOCKERFILE=Dockerfile.ubuntu.executable
+IMAGE_NAME=startim_executable
+CONTAINER_NAME=start_container_executable
+
 # Adding docker to the host X-server to turn on the graphical env
 xhost +local:docker
 
 # Build the Docker image
-docker build -t startim .
+docker build -f $DOCKERFILE -t $IMAGE_NAME .
 
 # Rebuild container from scratch each time. (2>/dev/null || true) avoid failure if in we have error in `stderr`
-docker stop start_container 2>/dev/null || true
-docker rm start_container 2>/dev/null || true
+docker stop $CONTAINER_NAME 2>/dev/null || true
+docker rm $CONTAINER_NAME 2>/dev/null || true
 
 # Run the Docker container with graphical environment support
-docker run -it --name start_container -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /dev/dri:/dev/dri startim
+docker run -it --name $CONTAINER_NAME -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /dev/dri:/dev/dri $IMAGE_NAME
 
 # To remove this image and container execute the following commands
-# docker rm start_container 2>/dev/null || true
-# docker rmi startim 2>/dev/null || true
+# docker rm $CONTAINER_NAME 2>/dev/null || true
+# docker rmi $IMAGE_NAME 2>/dev/null || true
 
 # To push image to the dockerhub:
 # docker login
-# docker tag startim <name_of_the_image_in_dockerhub>
-# docker push <name_of_the_image_in_dockerhub>
+# docker tag $IMAGE_NAME <dockerhub_user>/<image_name>:<tag>
+# docker push <dockerhub_user>/<image_name>:<tag>
