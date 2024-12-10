ce3a5960ebea6a4e63de7f6f6adbff54ccebbe24
ViNN280801 <vladislav_semykin01@mail.ru>
2024-12-10 16:38:52 +0300

    Changed unique_lock to the scoped_lock, added noexcept to allow more compiler optimizations for the recordMovement() method
    Signed-off-by: ViNN280801 <vladislav_semykin01@mail.ru>

--- src/ParticleInCellEngine/ParticleDynamicsProcessor/ParticleMovementTracker.cpp
+++ src/ParticleInCellEngine/ParticleDynamicsProcessor/ParticleMovementTracker.cpp
@@ -1,13 +1,13 @@
 #include "ParticleInCellEngine/ParticleDynamicsProcessor/ParticleMovementTracker.hpp"
 
 ParticleMovementTracker::ParticleMovementTracker(ParticleMovementMap &movements,
-                                                 std::mutex &mutex)
-    : m_particlesMovement(movements),
-      m_particlesMovementMutex(mutex) {}
+												 std::mutex &mutex)
+	: m_particlesMovement(movements),
+	  m_particlesMovementMutex(mutex) {}
 
-void ParticleMovementTracker::recordMovement(Particle const &particle, Point const &position, size_t maxParticles)
+void ParticleMovementTracker::recordMovement(Particle const &particle, Point const &position, size_t maxParticles) noexcept
 {
-    std::lock_guard<std::mutex> lock(m_particlesMovementMutex);
-    if (m_particlesMovement.size() <= maxParticles)
-        m_particlesMovement[particle.getId()].emplace_back(position);
+	std::scoped_lock lock(m_particlesMovementMutex);
+	if (m_particlesMovement.size() <= maxParticles)
+		m_particlesMovement[particle.getId()].emplace_back(position);
 }
