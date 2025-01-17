42bb4178a9af4e5755b60cfde41e4096e3727d11
ViNN280801 <vladislav_semykin01@mail.ru>
2025-01-17 14:35:00 +0300

    1st step completed: Creation of 3D model + AABB for the surface mesh
    Signed-off-by: ViNN280801 <vladislav_semykin01@mail.ru>

--- src/SputteringModel/TwoPlatesCreator.cpp
+++ src/SputteringModel/TwoPlatesCreator.cpp
@@ -2,13 +2,6 @@
 
 #include "SputteringModel/TwoPlatesCreator.hpp"
 
-void TwoPlatesCreator::_addPlates()
-{
-    gmsh::model::occ::addBox(0, 0, 0, 100 * m_unit, 20 * m_unit, 1 * m_unit);
-    gmsh::model::occ::addBox(0, 0, 100 * m_unit, 50 * m_unit, 20 * m_unit, 1 * m_unit);
-    gmsh::model::occ::synchronize();
-}
-
 std::vector<int> TwoPlatesCreator::_getAllBoundaryTags()
 {
     std::vector<int> all_boundary_tags;
@@ -37,7 +30,7 @@ void TwoPlatesCreator::_applyMesh()
     }
     else if (m_mesh_type == MeshType::Adaptive)
     {
-        auto boundary_tags = _getAllBoundaryTags();
+        auto boundary_tags{_getAllBoundaryTags()};
         _applyAdaptiveMesh(boundary_tags);
     }
 }
@@ -53,21 +46,53 @@ void TwoPlatesCreator::_applyAdaptiveMesh(const std::vector<int> &boundary_tags)
     gmsh::model::mesh::field::setNumber(2, "InField", 1.0);
     gmsh::model::mesh::field::setNumber(2, "SizeMin", 1.0 * m_unit);
     gmsh::model::mesh::field::setNumber(2, "SizeMax", 100.0 * m_unit);
-    gmsh::model::mesh::field::setNumber(2, "DistMin", 0.5 * m_unit);
-    gmsh::model::mesh::field::setNumber(2, "DistMax", 10.0 * m_unit);
+    gmsh::model::mesh::field::setNumber(2, "DistMin", 5.0 * m_unit);
+    gmsh::model::mesh::field::setNumber(2, "DistMax", 50.0 * m_unit);
 
     gmsh::model::mesh::field::setAsBackgroundMesh(2);
 }
 
-void TwoPlatesCreator::_generateMesh()
+TwoPlatesCreator::TwoPlatesCreator(MeshType mesh_type, double cell_size, double unit)
+    : m_mesh_type(mesh_type), m_cell_size(cell_size), m_unit(unit)
+{
+    static_assert(std::is_floating_point_v<decltype(cell_size)>, "cell_size must be a floating-point type.");
+    if (m_mesh_type == MeshType::Uniform && m_cell_size <= 0)
+        throw std::invalid_argument("Cell size must be greater than zero for uniform mesh.");
+}
+
+void TwoPlatesCreator::addPlates()
+{
+    m_volume_tags[0] = gmsh::model::occ::addBox(0, 0, 0, 100 * m_unit, 20 * m_unit, 1 * m_unit);
+    m_volume_tags[1] = gmsh::model::occ::addBox(20, 5, 100 * m_unit, 60 * m_unit, 10 * m_unit, 1 * m_unit);
+}
+
+void TwoPlatesCreator::assignMaterials(std::string_view material1_name, std::string_view material2_name)
+{
+    if (m_volume_tags.size() != 2)
+        throw std::runtime_error("Expected exactly two plates to assign materials.");
+    if (material1_name.length() < 1 || material1_name.length() > 2)
+        throw std::runtime_error("Expected material name with length (1 <= len <= 2) for the 'material1_name' param.");
+    if (material2_name.length() < 1 || material2_name.length() > 2)
+        throw std::runtime_error("Expected material name with length (1 <= len <= 2) for the 'material2_name' param.");
+
+    int material1_group{gmsh::model::addPhysicalGroup(3, {m_volume_tags.front()})};
+    gmsh::model::setPhysicalName(3, material1_group, std::string(material1_name));
+
+    int material2_group{gmsh::model::addPhysicalGroup(3, {m_volume_tags.back()})};
+    gmsh::model::setPhysicalName(3, material2_group, std::string(material2_name));
+}
+
+void TwoPlatesCreator::generateMeshfile()
 {
     try
     {
+        // Synchronize the OpenCASCADE model.
+        gmsh::model::occ::synchronize();
         _applyMesh();
         gmsh::model::mesh::generate(2);
-        gmsh::write("TwoPlates.msh");
+        gmsh::write(kdefault_mesh_filename);
 
-        std::cout << "Model successfully created and saved.\n";
+        std::cout << "Model successfully created and saved to the file '" << kdefault_mesh_filename << "'\n";
     }
     catch (const std::exception &ex)
     {
@@ -79,16 +104,18 @@ void TwoPlatesCreator::_generateMesh()
     }
 }
 
-TwoPlatesCreator::TwoPlatesCreator(MeshType mesh_type, double cell_size, double unit)
-    : m_mesh_type(mesh_type), m_cell_size(cell_size), m_unit(unit)
+void TwoPlatesCreator::show(int argc, char *argv[]) { m_gmsh_session.runGmsh(argc, argv); }
+
+std::string TwoPlatesCreator::getMaterial(int plate_number) const
 {
-    static_assert(std::is_floating_point_v<decltype(cell_size)>, "cell_size must be a floating-point type.");
-    if (m_mesh_type == MeshType::Uniform && m_cell_size <= 0)
-    {
-        throw std::invalid_argument("Cell size must be greater than zero for uniform mesh.");
-    }
-    _addPlates();
-    _generateMesh();
+    std::string material_name;
+    if (plate_number == 1)
+        gmsh::model::getPhysicalName(3, m_volume_tags.front(), material_name);
+    else if (plate_number == 2)
+        gmsh::model::getPhysicalName(3, m_volume_tags.back(), material_name);
+    else
+        throw std::runtime_error("Expected 'plate_number' == 1 or 'plate_number == 2'");
+    return material_name;
 }
 
-void TwoPlatesCreator::show(int argc, char *argv[]) { m_gmsh_session.runGmsh(argc, argv); }
+std::string TwoPlatesCreator::getMeshFilename() const { return kdefault_mesh_filename; }
--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,9 @@ int main(int argc, char *argv[])
     //     modeling.startModeling();
     // }
 
+    // // Kokkos and MPI are finalized automatically when tpetraScope goes out of scope.
+    // return EXIT_SUCCESS;
+
     [[maybe_unused]] std::string material("Ti"), gas("Ar"), scattering_model("VSS");
     [[maybe_unused]] double density{4500.0}, molar_mass{0.048}, cell_size = 0.0, mm = 1.0;
     [[maybe_unused]] int grid_type, particle_weight{10};
@@ -32,9 +35,31 @@ int main(int argc, char *argv[])
         std::cin >> cell_size;
     }
 
+    // 1. Creation of 2 plates.
     TwoPlatesCreator tpc(mesh_type, cell_size, mm);
+    tpc.addPlates();
+
+    // 2. Assigning materials to these plates.
+    tpc.assignMaterials("Ti", "Ni");
+    std::cout << "Материал подложки (большой пластинки): " << tpc.getMaterial(1) << '\n';
+    std::cout << "Материал мишени (маленькой пластинки): " << tpc.getMaterial(2) << '\n';
+
+    // 3. Generating mesh to the .msh file.
+    tpc.generateMeshfile();
+
+    // 4. Applying AABB tree to the surface mesh (with triangle cells).
+    auto surfaceMeshParams{Mesh::getMeshParams(tpc.getMeshFilename())};
+    auto surfaceMeshAABB{constructAABBTreeFromMeshParams(surfaceMeshParams)};
+    if (!surfaceMeshAABB)
+    {
+        ERRMSG("Failed to create AABB Tree for the surface mesh");
+    }
+    else
+    {
+        SUCCESSMSG("Created AABB Tree for the surface mesh");
+    }
+
     tpc.show(argc, argv);
 
-    // Kokkos and MPI are finalized automatically when tpetraScope goes out of scope.
     return EXIT_SUCCESS;
 }
